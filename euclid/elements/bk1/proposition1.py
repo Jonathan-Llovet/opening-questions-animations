98f58d40b9188c8522ad1c1b83ff9154d69cb9cf
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-18 18:33:08 -0400

    Refactor proposition 1 main diagram and add point labels

--- euclid/elements/bk1/proposition1.py
+++ euclid/elements/bk1/proposition1.py
@@ -1,40 +1,71 @@
 from manimlib.imports import *
-# from communitylib.utils.imports import *
-
 
 class Diagram1(Scene, GetIntersections):
     def construct(self):
-        pointA = np.array([-.5, 0, 0])
-        pointB = np.array([.5, 0, 0])
-        circleAB = Circle(arc_center=pointA)
-        circleBC = Circle(arc_center=pointB)
+        self.defineDiagram()
+        self.drawDiagram()
 
-        lineAB = Line(start=pointA, end=pointB)
-        intersections = self.get_intersections_between_two_vmobs(circleAB, circleBC)
+    def drawDiagram(self):
+        self.markPoint(self.pointA, self.pointA_label)
+        self.play(ShowCreation(self.circleAB))
 
-        pointC = None
-        for point in intersections:
-            # This will only work in this case, where there is only one valid intersection
-            print(point)
-            if point[1] > 0:
-                pointC = point
+        self.markPoint(self.pointB, self.pointB_label)
+        self.play(ShowCreation(self.lineAB))
 
-        lineAC = Line(start=pointC, end=pointA)
-        lineBC = Line(start=pointC, end=pointB)
+        self.circleBC.flip(UP) # Draws from right side, going up
+        # self.circleBC.rotate(DEGREES*180) # Draws from left side, going down
+        self.play(ShowCreation(self.circleBC))
+        self.wait()
+        self.markPoint(self.pointC, self.pointC_label)
 
-        self.markPoint(pointA)
-        self.play(ShowCreation(circleAB))
-        self.markPoint(pointB)
-        self.play(ShowCreation(circleBC))
-        self.play(ShowCreation(lineAB))
-        self.markPoint(pointC)
-        self.play(ShowCreation(lineAC))
-        self.play(ShowCreation(lineBC))
-        # self.play(ShowCreation(triangle))
+        self.play(ShowCreation(self.lineAC))
+        self.play(ShowCreation(self.lineBC))
 
         self.wait()
 
-    def markPoint(self, point):
+    def defineDiagram(self):
+        self.pointA = np.array([-1, 0, 0])
+        self.pointA_label = TexMobject("A")
+        self.pointA_label.next_to(self.pointA, LEFT)
+
+        self.circleAB_radius = 2
+
+        self.pointB = self.pointA.copy()
+        self.pointB[0] += self.circleAB_radius # X coordinate offset
+        self.pointB_label = TexMobject("B")
+        self.pointB_label.next_to(self.pointB, RIGHT)
+
+        self.circleAB = Circle(
+            arc_center=self.pointA,
+            radius=self.circleAB_radius
+            )
+        self.circleBC = Circle(
+            arc_center=self.pointB,
+            radius=self.circleAB_radius
+            )
+
+        self.lineAB = Line(start=self.pointA, end=self.pointB)
+
+        self.intersections = self.get_intersections_between_two_vmobs(
+            self.circleAB,
+            self.circleBC
+            )
+
+        self.pointC = None
+        for point in self.intersections:
+            # This will only work in this case, where there is only one valid intersection
+            # print(point)
+            if point[1] > 0: # checks y coordinate
+                self.pointC = point
+
+        self.pointC_label = TexMobject("C")
+        self.pointC_label.next_to(self.pointC, UP)
+
+        self.lineAC = Line(start=self.pointC, end=self.pointA)
+        self.lineBC = Line(start=self.pointC, end=self.pointB)
+
+    def markPoint(self, point, label):
         dot = Dot(radius=0.05, color=YELLOW, point=point)
-        self.play(ShowCreation(dot))
+        self.play(ShowCreation(dot), ShowCreation(label))
         self.add_foreground_mobjects(dot)
+        self.add_foreground_mobjects(label)
