84534aa682eb6c80a5ba1bad2756c0ecea381281
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-19 18:54:15 -0400

    Start defining intro scene

---
+++ euclid/elements/shared_scenes/intro.py
@@ -0,0 +1,49 @@
+from manimlib.imports import *
+
+class EuclidIntro(Scene):
+    def construct(self):
+
+        euclid_greek = Text(
+            "τὰ Στοιχεῖα Εὐκλείδου",
+            font="Georgia",
+            stroke_width=0,
+            size=1
+            )
+
+        euclid_english = Text(
+            "Euclid's Elements",
+            font="Georgia",
+            stroke_width=0,
+            size=1
+            )
+
+        opening_questions_english = Text(
+            "Opening Questions",
+            font="Georgia",
+            stroke_width=0,
+            size=1
+        )
+        placeholder = Text(
+            "placeholder",
+            font="Georgia",
+            stroke_width=0,
+            size=1
+        )
+
+        self.play(Write(opening_questions_english))
+        self.wait()
+        self.play(ReplacementTransform(opening_questions_english, placeholder), run_time=2)
+        self.wait()
+        self.play(ReplacementTransform(placeholder, euclid_greek), run_time=2)
+        self.wait()
+        self.play(ReplacementTransform(euclid_greek, euclid_english), run_time=2)
+        self.wait()
+
+    def construct_math_diagram(self):
+        pass
+
+    def construct_programming_diagram(self):
+        pass
+
+    def construct_philosophy_diagram(self):
+        pass
