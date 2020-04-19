b5fb4fe57db75f80bb66f58d8e3ffa090a5b247a
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-19 13:39:29 -0400

    Add triangleABC and refactor helper functions

--- euclid/elements/bk1/proposition1.py
+++ euclid/elements/bk1/proposition1.py
@@ -3,28 +3,49 @@ from manimlib.imports import *
 class Diagram1(Scene, GetIntersections):
     def construct(self):
         self.defineDiagram()
+        self.defineEquilateralTriangle()
         self.drawDiagram()
 
     def drawDiagram(self):
-        self.markPoint(self.pointA, self.pointA_label)
+
+        self.play(ShowCreation(self.triangleABC, run_time=3))
+        self.wait()
+        self.play(Uncreate(self.triangleABC))
+
+        self.markPoint(self.dotA, self.pointA_label)
         self.play(ShowCreation(self.circleAB))
 
-        self.markPoint(self.pointB, self.pointB_label)
+        self.markPoint(self.dotB, self.pointB_label)
         self.play(ShowCreation(self.lineAB))
 
-        self.circleBC.flip(UP) # Draws from right side, going up
-        # self.circleBC.rotate(DEGREES*180) # Draws from left side, going down
         self.play(ShowCreation(self.circleBC))
         self.wait()
-        self.markPoint(self.pointC, self.pointC_label)
+        self.markPoint(self.dotC, self.pointC_label)
 
         self.play(ShowCreation(self.lineAC))
         self.play(ShowCreation(self.lineBC))
 
         self.wait()
 
+    def defineEquilateralTriangle(self):
+        self.triangleABC = VGroup(
+            # self.pointA,
+            self.dotA.copy(),
+            self.pointA_label.copy(),
+            # self.pointB,
+            self.dotB.copy(),
+            self.pointB_label.copy(),
+            self.lineAB.copy(),
+            # self.pointC,
+            self.dotC.copy(),
+            self.pointC_label.copy(),
+            self.lineAC.copy(),
+            self.lineBC.copy()
+        )
+
     def defineDiagram(self):
         self.pointA = np.array([-1, 0, 0])
+        self.dotA = self.createDotForPoint(self.pointA)
         self.pointA_label = TexMobject("A")
         self.pointA_label.next_to(self.pointA, LEFT)
 
@@ -32,6 +53,7 @@ class Diagram1(Scene, GetIntersections):
 
         self.pointB = self.pointA.copy()
         self.pointB[0] += self.circleAB_radius # X coordinate offset
+        self.dotB = self.createDotForPoint(self.pointB)
         self.pointB_label = TexMobject("B")
         self.pointB_label.next_to(self.pointB, RIGHT)
 
@@ -43,6 +65,8 @@ class Diagram1(Scene, GetIntersections):
             arc_center=self.pointB,
             radius=self.circleAB_radius
             )
+        self.circleBC.flip(UP) # Draws from right side, going up
+        # self.circleBC.rotate(DEGREES*180) # Draws from left side, going down
 
         self.lineAB = Line(start=self.pointA, end=self.pointB)
 
@@ -57,15 +81,21 @@ class Diagram1(Scene, GetIntersections):
             # print(point)
             if point[1] > 0: # checks y coordinate
                 self.pointC = point
-
+        self.dotC = self.createDotForPoint(self.pointC)
         self.pointC_label = TexMobject("C")
         self.pointC_label.next_to(self.pointC, UP)
 
         self.lineAC = Line(start=self.pointC, end=self.pointA)
         self.lineBC = Line(start=self.pointC, end=self.pointB)
 
-    def markPoint(self, point, label):
-        dot = Dot(radius=0.05, color=YELLOW, point=point)
-        self.play(ShowCreation(dot), ShowCreation(label))
+
+    def createDotForPoint(self, point):
+        return Dot(radius=0.05, color=YELLOW, point=point)
+
+    def setDotAndLabelInForeground(self, dot, label):
         self.add_foreground_mobjects(dot)
         self.add_foreground_mobjects(label)
+
+    def markPoint(self, dot, label):
+        self.play(ShowCreation(dot), ShowCreation(label))
+        self.setDotAndLabelInForeground(dot, label)
