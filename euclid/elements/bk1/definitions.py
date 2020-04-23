de35e267ff14b1d7936e15d732426cfb503827bd
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-23 19:10:24 -0400

    Define structure for holding multi-language-texts

---
+++ euclid/elements/bk1/definitions.py
@@ -0,0 +1,21 @@
+header = {
+    "greek": "Ὅροι",
+    "latin": "Definitiones",
+    "english": "Definitions"
+}
+
+definitions = [
+    {
+        "text": {
+            "greek": "σημεῖόν ἐστιν, οὗ μέρος οὐθέν.",
+            "latin": "Punctum est, cuius pars nulla est.",
+            "english": "A point is that which has no part"
+        },
+        "commentary": {
+            "greek": "",
+            "latin": "",
+            "english": ""
+        }
+    },
+    {}
+    ]
