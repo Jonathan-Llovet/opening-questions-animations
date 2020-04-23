ce9f71eedbdb7a5eff466808e001b57f41fdc1ff
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-22 21:00:56 -0400

    Update component names and reorder animations for BigOLogo

--- logos/big_o_notation_logo.py
+++ logos/big_o_notation_logo.py
@@ -2,13 +2,13 @@ from manimlib.imports import *
 
 class BigOLogo(Scene):
     def construct(self):
-        header = TextMobject("Opening Questions").move_to(DOWN*1.25)
-        openingQuestions = TexMobject("O", "(", "Q", ")").scale_in_place(3)
-        openingQuestions.set_color_by_tex("O", BLUE_B)
-        openingQuestions.set_color_by_tex("Q", WHITE)
-        openingQuestions.set_color_by_tex("(", RED_E)
-        openingQuestions.set_color_by_tex(")", RED_E)
-        logo = VGroup(header, openingQuestions).move_to(np.array([0, 0, 0])).scale_in_place(1.45)
-        self.play(ShowCreation(logo))
-        # self.play(ShowCreation(header))
-        # self.play(ShowCreation(openingQuestions))
+        opening_questions = TextMobject("Opening Questions").move_to(DOWN*1.25)
+        bigO = TexMobject("O", "(", "Q", ")").scale_in_place(3)
+        bigO.set_color_by_tex("O", BLUE_B)
+        bigO.set_color_by_tex("Q", WHITE)
+        bigO.set_color_by_tex("(", RED_E)
+        bigO.set_color_by_tex(")", RED_E)
+        logo = VGroup(bigO, opening_questions).move_to(np.array([0, 0, 0])).scale_in_place(1.45)
+        self.play(ShowCreation(bigO), run_time=2)
+        self.play(ShowCreation(opening_questions), run_time=2)
+        self.wait()
