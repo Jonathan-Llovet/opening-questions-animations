0d00149eb0eb0bde074dc6b7bcc8e71598b5e505
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-23 22:43:48 -0400

    Update example access to json data for definitions

--- euclid/elements/bk1/definitions.py
+++ euclid/elements/bk1/definitions.py
@@ -5,4 +5,4 @@ definitions_path = './definitions.json'
 with open(definitions_path, 'r') as f:
     definitions = json.load(f)
 
-# print(definitions['definitions'][2]['text']['greek'])
+# print(definitions['definitions'][2]['text']['grc'])
