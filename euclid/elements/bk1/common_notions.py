1f2fa3ebf44c060a19af0cc3f57719a0b6eece22
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-04-23 21:52:34 -0400

    Move common_notions data to json and add imports to common_notions.py

--- euclid/elements/bk1/common_notions.py
+++ euclid/elements/bk1/common_notions.py
@@ -1,9 +1,8 @@
-header = {
-    "greek": "κοιναὶ ἔννοιαι",
-    "latin": "Communes Animi Conceptiones",
-    "english": "Common Notions"
-}
+import json
 
-common_notions = [
-    {},
-]
+common_notions_path = './common_notions.json'
+
+with open(common_notions_path, 'r') as f:
+    common_notions = json.load(f)
+
+# print(common_notions['common_notions'][2]['text']['greek'])
